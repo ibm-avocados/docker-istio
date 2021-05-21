fdd1b995a019c1b24e50acec2680129eb3cbfedd
Oliver Rodriguez <oliverd.rodriguez@gmail.com>
2021-05-21 11:43:29 -0500

    added istioctl to script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ISTIO_VERSION="1.7.2"
+ISTIO_VERSION="${4}"
+
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${ISTIO_VERSION} TARGET_ARCH=x86_64 sh -
+
+cd istio-${ISTIO_VERSION}
+
+export PATH=${PATH}:$PWD/bin
 
 ibmcloud plugin install -f kubernetes-service
 sleep 2
@@ -8,8 +14,8 @@ ibmcloud login --apikey "${1}" -r "us-south"
 sleep 2
 ibmcloud ks cluster config --cluster $2
 sleep 2
-"/opt/istio-${ISTIO_VERSION}/bin/istioctl" install --set profile=demo
+istioctl install --set profile=demo
 sleep 2
 kubectl label namespace $3 istio-injection=enabled
 sleep 2
-kubectl apply -f "/opt/istio-${ISTIO_VERSION}/samples/bookinfo/platform/kube/bookinfo.yaml"
+kubectl apply -f "./samples/bookinfo/platform/kube/bookinfo.yaml"
