bc4317bf6b85827243522257d16323e62d2b0186
Oliver Rodriguez <oliverd.rodriguez@gmail.com>
2021-05-21 11:33:48 -0500

    shortening sleep

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,13 +3,13 @@
 ISTIO_VERSION="1.7.2"
 
 ibmcloud plugin install -f kubernetes-service
-sleep 20
+sleep 2
 ibmcloud login --apikey "${1}" -r "us-south"
-sleep 20
+sleep 2
 ibmcloud ks cluster config --cluster $2
-sleep 20
+sleep 2
 "/opt/istio-${ISTIO_VERSION}/bin/istioctl" install --set profile=demo
-sleep 20
+sleep 2
 kubectl label namespace $3 istio-injection=enabled
-sleep 20
+sleep 2
 kubectl apply -f "/opt/istio-${ISTIO_VERSION}/samples/bookinfo/platform/kube/bookinfo.yaml"
