8efe6592f38e3f551cdd49156a0fc0fdb62de0de
Oliver Rodriguez <oliverd.rodriguez@gmail.com>
2021-05-21 11:11:28 -0500

    removed region

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ ISTIO_VERSION="1.7.2"
 
 ibmcloud plugin install -f kubernetes-service
 sleep 20
-ibmcloud login --apikey $1 -r "us-south"
+ibmcloud login --apikey $1
 sleep 20
 ibmcloud ks cluster config --cluster $2
 sleep 20
